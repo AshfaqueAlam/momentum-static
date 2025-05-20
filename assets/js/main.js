d16eaba0f183953e031e7da8949aab66d4021d37
Ashfaque Alam <82508902+AshfaqueAlam@users.noreply.github.com>
2025-05-20 16:29:01 +0530

    updated

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+data = {
+  genre1: {
+    displayName: "Genre 1",
+    description: "Description of Genre 1",
+    thumbnailPath: "assets/images/placeholders/test_placeholder.jpg",
+    videos: [
+      {
+        offlinePath: "assets/videos/SampleVideo_1280x720_1mb.mp4",
+        onlineUrl: "https://www.youtube.com/watch?v=cNJU2TvLkY0",
+        videoThumbnailPath: "assets/images/thumbnails/SampleFile_Thumbnail.jpg",
+      },
+      {
+        offlinePath: "assets/videos/SampleVideo_1280x720_1mb.mp4",
+        onlineUrl: "https://www.youtube.com/watch?v=cNJU2TvLkY0",
+        videoThumbnailPath: "assets/images/thumbnails/SampleFile_Thumbnail.jpg",
+      },
+    ],
+  },
+    genre2: {
+    displayName: "Genre 2",
+    description: "Description of Genre 2",
+    thumbnailPath: "assets/images/placeholders/test_placeholder.jpg",
+    videos: [
+      {
+        offlinePath: "assets/videos/SampleVideo_1280x720_1mb.mp4",
+        onlineUrl: "https://www.youtube.com/watch?v=cNJU2TvLkY0",
+        videoThumbnailPath: "assets/images/thumbnails/SampleFile_Thumbnail.jpg",
+      },
+      {
+        offlinePath: "assets/videos/SampleVideo_1280x720_1mb.mp4",
+        onlineUrl: "https://www.youtube.com/watch?v=cNJU2TvLkY0",
+        videoThumbnailPath: "assets/images/thumbnails/SampleFile_Thumbnail.jpg",
+      },
+    ],
+  },
+};
