af5e31c8c4fc7a958324075a3dedfc6c812732be
Ashfaque Alam <82508902+AshfaqueAlam@users.noreply.github.com>
2025-12-27 19:47:43 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,18 @@ const data = {
         onlineUrl: "https://www.instagram.com/reel/DIL4N1kzNxm/?igsh=MWN2OXV0N2J5bmRocg==",
         // videoThumbnailPath: "assets/images/thumbnails/SampleFile_Thumbnail.jpg",
       },
+      {
+        offlinePath: "assets/videos/warmup2.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DHBNHptIJV6/?igsh=YzBmOGRsMHA3NHls",
+      },
+      {
+        offlinePath: "assets/videos/warmup3.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DJe3v18xHmN/?igsh=MWd3NXEzYmE3YXZvNg==",
+      },
+      {
+        offlinePath: "assets/videos/warmup4.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DFrwMTkI7VG/?igsh=MTlkcThseGU4MGM0bg==",
+      },
     ],
   },
   mobility: {
@@ -50,6 +62,30 @@ const data = {
         offlinePath: "assets/videos/mobility8.mp4",
         onlineUrl: "https://www.instagram.com/reel/DIrL7HYynAC/?igsh=OWE1ejlvdm53enc=",
       },
+      {
+        offlinePath: "assets/videos/mobility9.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DGts_8VSr6T/?igsh=MWhsMm9zOXZ4cm94ZQ==",
+      },
+      {
+        offlinePath: "assets/videos/mobility10.mp4",
+        onlineUrl: "https://www.instagram.com/p/DLfmqU5xR1x/?img_index=2&igsh=MzUzc241M3YyY2t2",
+      },
+      {
+        offlinePath: "assets/videos/mobility11.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DJenRz9qMbL/?igsh=MWFxd3FzMGlpM3Z2Zw==",
+      },
+      {
+        offlinePath: "assets/videos/mobility12.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DHLr6TrCq0z/",
+      },
+      {
+        offlinePath: "assets/videos/mobility13.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DE8ErcQyWSC/?igsh=MXY3MXB4ZjJvcHY2",
+      },
+      {
+        offlinePath: "assets/videos/mobility14.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DJW4DqDzI7e/?igsh=dHB3c21hMnUyeDFx",
+      },
     ],
   },
   chest: {
@@ -78,13 +114,34 @@ const data = {
             offlinePath: "assets/videos/chest5.mp4",
             onlineUrl: "https://www.instagram.com/reel/DJegtv4IUwb/?igsh=amt6dzU4OXY2NHVq",
           },
+          {
+            offlinePath: "assets/videos/chest6.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DLc5_vMxlHY/?igsh=Z3h6aGpsdzJwMTJ3",
+          },
+          {
+            offlinePath: "assets/videos/chest9.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DMEu_nSJt_s/?igsh=OGVkZTBleXJ2MjZl",
+          },
+          {
+            offlinePath: "assets/videos/chest10.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DJy7kbng8qZ/?igsh=MTRrbWhveGh6cHk5cQ==",
+          },
+          {
+            offlinePath: "assets/videos/chest11.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DFO_6VQRNEf/?igsh=MWJpZ3ppNzhld2p1ZQ==",
+          },
         ],
       },
       upperChest: {
         displayName: "Upper Chest (Clavicular Head)",
         description: "Upper chest exercises to target the upper pectoral muscles.",
         thumbnailPath: "assets/images/placeholders/chest1.png",
-        videos: [],
+        videos: [
+          {
+            offlinePath: "assets/videos/chest7.mp4",
+            onlineUrl: "https://www.instagram.com/p/DSXoyM5CM3C/?igsh=MWtpMjFncGhqNDRjeA==",
+          },
+        ],
       },
       lowerChest: {
         displayName: "Lower Chest (Abdominal Head)",
@@ -94,6 +151,10 @@ const data = {
           {
             offlinePath: "assets/videos/chest3.mp4", // ! Startcut
             onlineUrl: "https://youtube.com/shorts/NykyCcxIfY4?feature=shared",
+          },
+          {
+            offlinePath: "assets/videos/chest8.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DIb1881MKmr/?igsh=MWJnNHlnNXl3azRrdg==",
           },
         ],
       },
@@ -125,6 +186,22 @@ const data = {
             offlinePath: "assets/videos/back5.mp4",
             onlineUrl: "https://www.instagram.com/reel/DIvuRlwsvZT/?igsh=d2RkODlnMmcxMnVn",
           },
+          {
+            offlinePath: "assets/videos/back6.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DQzP3ypEnpI/?igsh=MTllOGl0MmpwbjkyeA==",
+          },
+          {
+            offlinePath: "assets/videos/back7.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DK_rM9eM9vh/?igsh=MW40dmxyYmtxNGw4NQ==",
+          },
+          {
+            offlinePath: "assets/videos/back11.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DRAGivSD4C4/?igsh=NXNkZzdqbzRnbGJ6",
+          },
+          {
+            offlinePath: "assets/videos/back12.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DJ9Lc7DN4TO/?igsh=dTRwcHlmMXFtM3Rk",
+          },
         ],
       },
       traps: {
@@ -135,6 +212,14 @@ const data = {
           {
             offlinePath: "assets/videos/shoulder1.mp4",
             onlineUrl: "https://youtube.com/shorts/7AQ8ZyMCOvM?feature=shared",
+          },
+          {
+            offlinePath: "assets/videos/back8.mp4",
+            onlineUrl: "https://www.instagram.com/reel/CpBEQlRsEVH/?igsh=MWt4NXhkbXRjZDNjYg==",
+          },
+          {
+            offlinePath: "assets/videos/back13.mp4",
+            onlineUrl: "https://www.instagram.com/p/DL0RWG8Iqt4/?img_index=1&igsh=NWR1OHltcGV5NzE4",
           },
         ],
       },
@@ -147,13 +232,26 @@ const data = {
             offlinePath: "assets/videos/back1.mp4", // ! Edit: Keep Last workout
             onlineUrl: "https://youtube.com/shorts/u-icTznUuz8?feature=shared",
           },
+          {
+            offlinePath: "assets/videos/back9.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DOt8NaGEmHC/?igsh=MTl0ZmVteHRoOTl1dw==",
+          },
         ],
       },
       lowerBack: {
         displayName: "Lower Back (Erector Spinae)",
         description: "Lower back exercises to strengthen and tone your erector spinae muscles.",
         thumbnailPath: "assets/images/placeholders/back1.png",
-        videos: [],
+        videos: [
+          {
+            offlinePath: "assets/videos/back10.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DQ-GsT0E2O-/?igsh=M3Jid3lnamIwNXk1",
+          },
+          {
+            offlinePath: "assets/videos/back14.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DFdExrrOBKb/?igsh=c3Izd2l5YXpsOWJ5",
+          },
+        ],
       },
     },
   },
@@ -175,6 +273,14 @@ const data = {
             offlinePath: "assets/videos/shoulder2.mp4", // ! Edit: Keep First 2 workout
             onlineUrl: "https://youtube.com/shorts/guWmxcdmnwc?feature=shared",
           },
+          {
+            offlinePath: "assets/videos/shoulder5.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DO6X87LDf4_/?igsh=cmJlc3BheTJnaGts",
+          },
+          {
+            offlinePath: "assets/videos/shoulder8.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DH6RxJOoMo0/?igsh=MTY4NmVhNzg2bHFw",
+          },
         ],
       },
       sideDelt: {
@@ -186,6 +292,10 @@ const data = {
             offlinePath: "assets/videos/shoulder2.mp4",
             onlineUrl: "https://youtube.com/shorts/guWmxcdmnwc?feature=shared",
           },
+          {
+            offlinePath: "assets/videos/shoulder6.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DHn_HvsTsrh/?igsh=MXRzaWVzcWR6Nzlmcg==",
+          },
         ],
       },
       rearDelt: {
@@ -196,6 +306,10 @@ const data = {
           {
             offlinePath: "assets/videos/shoulder4.mp4", // ! Edit: Keep Last workout
             onlineUrl: "https://www.instagram.com/reel/DJ6g6Ycs_4f/?igsh=MWprdXd0MGFiYmVwbg==",
+          },
+          {
+            offlinePath: "assets/videos/shoulder7.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DO6X87LDf4_/?igsh=cmJlc3BheTJnaGts",
           },
         ],
       },
@@ -225,6 +339,10 @@ const data = {
             offlinePath: "assets/videos/arm2.mp4",
             onlineUrl: "https://youtube.com/shorts/IwGvls2g4p0?feature=shared",
           },
+          {
+            offlinePath: "assets/videos/arm10.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DIhCSR9zw9P/?igsh=cTZ0NjhkNzVpMTR4",
+          },
         ],
       },
       tricep: {
@@ -248,6 +366,10 @@ const data = {
             offlinePath: "assets/videos/arm6.mp4",
             onlineUrl: "https://www.instagram.com/reel/DHJK0K9uBKi/?igsh=dm85OTE1ZjBhNTRi",
           },
+          {
+            offlinePath: "assets/videos/arm11.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DPOk2rrDZXM/?igsh=MWUzOWR6Z2hjY2l2bg==",
+          },
         ],
       },
       forearm: {
@@ -266,6 +388,14 @@ const data = {
           {
             offlinePath: "assets/videos/arm9.mp4",
             onlineUrl: "https://youtube.com/shorts/d5YiFNoiCa0?feature=shared",
+          },
+          {
+            offlinePath: "assets/videos/arm12.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DGRggNmRKGP/?igsh=cjAzeTl2cmxnMnJl",
+          },
+          {
+            offlinePath: "assets/videos/arm13.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DNYbHXvBdEO/?igsh=dDhpcmhsajdwanZv",
           },
         ],
       },
@@ -297,13 +427,26 @@ const data = {
             offlinePath: "assets/videos/leg4.mp4",
             onlineUrl: "https://youtube.com/shorts/AqiS1Qe_drQ?feature=shared",
           },
+          {
+            offlinePath: "assets/videos/leg12.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DMbiQb9JJKc/?igsh=ZzR5cTB6azk4Ym01",
+          },
+          {
+            offlinePath: "assets/videos/leg16.mp4",
+            onlineUrl: "https://www.instagram.com/p/DLzJzYcNmSo/",
+          },
         ],
       },
       hamstrings: {
         displayName: "Hamstrings",
         description: "Hamstring exercises to strengthen and tone your hamstrings muscles.",
         thumbnailPath: "assets/images/placeholders/thigh1.jpg",
-        videos: [],
+        videos: [
+          {
+            offlinePath: "assets/videos/leg13.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DIEX61QTzej/?igsh=MWhwOWs1aXBoZDV4eA==",
+          },
+        ],
       },
       glute: {
         displayName: "Glute",
@@ -326,6 +469,14 @@ const data = {
             offlinePath: "assets/videos/leg8.mp4",
             onlineUrl: "https://youtube.com/shorts/Fbw_98aWeA8?feature=shared",
           },
+          {
+            offlinePath: "assets/videos/leg18.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DHnFOTYpZ1P/?igsh=MWY0aThsa2I3cHpuNA==",
+          },
+          {
+            offlinePath: "assets/videos/leg19.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DK7PjkARECU/?igsh=MTJqMmkxd2VuOTlkOQ==",
+          },
         ],
       },
       calves: {
@@ -344,6 +495,25 @@ const data = {
           {
             offlinePath: "assets/videos/leg11.mp4",
             onlineUrl: "https://youtube.com/shorts/oE6SQanl5e4?feature=shared",
+          },
+          {
+            offlinePath: "assets/videos/leg14.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DPwFsaiD8QG/?igsh=NTNjZXN1aGc1ZzA5",
+          },
+          {
+            offlinePath: "assets/videos/leg17.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DLOOwmszSab/?igsh=Nmdja2cyNXBjNWJo",
+          },
+        ],
+      },
+      abductor: {
+        displayName: "Abductor",
+        description: "Abductor exercises to strengthen and tone your abductor muscles.",
+        thumbnailPath: "assets/images/placeholders/thigh1.jpg",
+        videos: [
+          {
+            offlinePath: "assets/videos/leg15.mp4", // ! Edit: Keep Last workout
+            onlineUrl: "https://www.instagram.com/reel/DOoI1rLjod9/?igsh=YjJ4YjZqaHNuczYz",
           },
         ],
       },
@@ -383,13 +553,30 @@ const data = {
             offlinePath: "assets/videos/core6.mp4",
             onlineUrl: "https://www.instagram.com/reel/DGvGtw0InUN/?igsh=MXc1bWM0Mm9neTVwdg==",
           },
+          {
+            offlinePath: "assets/videos/core10.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DR4tCiUjt7F/?igsh=MWExcGphdjIyMHRhZw==",
+          },
+          {
+            offlinePath: "assets/videos/core14.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DHBFQ6LMRO_/?igsh=MTBiNjVlc2tuM2J4Mg==",
+          },
+          {
+            offlinePath: "assets/videos/core15.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DI_uZrIMNyt/?igsh=eXh0M2I2dHhsdXVy",
+          },
         ],
       },
       obliques: {
         displayName: "Obliques",
         description: "Oblique exercises to strengthen and tone your oblique muscles.",
         thumbnailPath: "assets/images/placeholders/abs1.jpg",
-        videos: [],
+        videos: [
+          {
+            offlinePath: "assets/videos/core11.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DQ8Ia0Qjkzx/?igsh=MXh3djk1N296OXpjcA==",
+          },
+        ],
       },
       pelvicFloor: {
         displayName: "Pelvic Floor",
@@ -408,6 +595,14 @@ const data = {
             offlinePath: "assets/videos/core9.mp4",
             onlineUrl: "https://youtube.com/shorts/Rlv0zQO-4Kg?feature=shared",
           },
+          {
+            offlinePath: "assets/videos/core12.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DEy1Vc_zajJ/?igsh=MWpwMHhrenVvM2R4cg==",
+          },
+          {
+            offlinePath: "assets/videos/core13.mp4",
+            onlineUrl: "https://www.instagram.com/reel/DKcFcvBOM0z/?igsh=OW1mZzJjbW9ndmI0",
+          },
         ],
       },
     },
@@ -419,7 +614,46 @@ const data = {
     videos: [
       {
         offlinePath: "assets/videos/calisthenics1.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DLxDKEHo5qu/?igsh=MTc0b2V0NzNiZGpzYw==",
+      },
+      {
+        offlinePath: "assets/videos/calisthenics2.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DK18tK9ITCr/?igsh=aWFyMTc3cml4emNo",
+      },
+      {
+        offlinePath: "assets/videos/calisthenics3.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DJmN7JnNr6J/?igsh=bmRmNmdmY2RzN3Yw",
+      },
+      {
+        offlinePath: "assets/videos/calisthenics4.mp4",
         onlineUrl: "https://www.instagram.com/reel/DGvuaHxoeWS/?igsh=MWVsNGdsNHdxeDFwZA==",
+      },
+    ],
+  },
+  bestPractices: {
+    displayName: "Best Practices",
+    description: "Tips and best practices for effective workouts.",
+    thumbnailPath: "assets/images/placeholders/bestPractices1.png",
+    videos: [
+      {
+        offlinePath: "assets/videos/bestPractices1.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DEaC008AU_n/?igsh=bTRwZDBxcjRxdDVu",
+      },
+      {
+        offlinePath: "assets/videos/bestPractices2.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DGTphq8M4Be/?igsh=MWh0aHNlMXJ4eDQweQ==",
+      },
+      {
+        offlinePath: "assets/videos/bestPractices3.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DKrj-95RBiZ/?igsh=MTllbHQ4cWxkMDhhNA==",
+      },
+      {
+        offlinePath: "assets/videos/bestPractices4.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DIwhUTZTLwl/?igsh=NjB1anZzOXY0bjU4",
+      },
+      {
+        offlinePath: "assets/videos/bestPractices5.mp4",
+        onlineUrl: "https://www.instagram.com/reel/DJuJWI_yCpk/?igsh=MXE1bDgxaHl0cDRxdA==",
       },
     ],
   },
